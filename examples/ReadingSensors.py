cbed7c748175c43a4915f1fdfc0eba8fce6a135a
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2020-04-23 14:59:14 +0200

    Add license to example application

--- examples/ReadingSensors.py
+++ examples/ReadingSensors.py
@@ -1,3 +1,27 @@
+'''
+ * ReadingSensors.py
+ *
+ *  Copyright (C) Daniel Kampert, 2020
+ *	Website: www.kampis-elektroecke.de
+ *  File info: Example application for the Python I2C pressure sensor module.
+
+  GNU GENERAL PUBLIC LICENSE:
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+  Errors and commissions should be reported to DanielKampert@kampis-elektroecke.de
+'''
+
 import time
 import bmp_sensors as Sensors
 
